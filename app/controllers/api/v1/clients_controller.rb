12d9684c1ae51a4d60e294add73cebc55204ce06
Vincent Chan <vchan313@gmail.com>
2019-03-14 19:04:31 -0400

    minor changes

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,7 +1,27 @@
 class Api::V1::ClientsController < ApplicationController
   
-  def index
+def index
   @clients = Client.all
   render json: @clients, status: :ok
   end
+
+
+def create
+	@client = Client.create(client_params)
+	render json: @client, status: :created
+end
+
+def destroy
+    @client = Client.find(params[:id])
+    @Client.destroy
+    render json: {message: "Client record deleted."}, status: :ok
+end
+
+
+private 
+
+def client_params
+params.require(:client).permit(:name, :email, :number, :user_id)
+end
+
 end
