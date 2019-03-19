e9fdc456c10d64b339e9eaf8bc9a6ca85d586e02
Vincent Chan <vchan313@gmail.com>
2019-03-19 13:14:43 -0400

    some changes to the serializer

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -5,6 +5,11 @@ def index
   render json: @clients, status: :ok
   end
 
+def show
+  @client = Client.find(params[:id])
+  render json: @client, status: :ok
+end
+
 
 def create
 	@client = Client.create(client_params)
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::UsersController < ApplicationController
   @users = User.all
   render json: @users, status: :ok
   end
+
+  def show
+  @user = User.find(params[:id])
+  render json: @user, status: :ok
+  end
+
 end
